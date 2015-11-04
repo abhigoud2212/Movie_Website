fd9a67a1faab465226641c2fe06f293290d674f9
Your Name <you@example.com>
2015-11-04 11:26:40 -0500

    edited by me

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Movie_Website
+
 Website with different movie names, When you click the image of the movie trailer starts playing
+To open the website. load all the files to your IDLE. 
+Run the file entertainment_center, This will open the web page in your browser.
+Created seperate files for the stories and Url links, where stories and url links for all the movies can be found.
---
+++ media.py
@@ -0,0 +1,16 @@
+__author__ = 'abhig'
+import webbrowser
+
+"""created the movie class with instance variables """
+
+class Movie():
+    def __init__(self,movie_title,movie_storyline,poster_image,youtube_trailer):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = youtube_trailer
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+
+
